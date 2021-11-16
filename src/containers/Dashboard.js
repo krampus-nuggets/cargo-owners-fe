4d679830298e1c35ff64d8742cf030fb3c0ac7ad
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-16 17:23:43 +0200

    add: dashboard container

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RatesContainer from "../components/dashboard/RatesContainer";
+import Section from "../components/global/Section";
+
+const Dashboard = () => {
+    return (
+        <Section>
+            <RatesContainer />
+        </Section>
+    );
+}
+
+export default Dashboard;
