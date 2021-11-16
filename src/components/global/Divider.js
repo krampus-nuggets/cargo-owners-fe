a94a16d8af7e177b1bd6551985d8ac1e1e5705a4
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-16 17:23:33 +0200

    add: card info divider

---
+++ src/components/global/Divider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Divider = () => {
+    return (
+        <div className="w-full border-solid border-black border-b-2 border-l-0 mb-2"></div>
+    );
+}
+
+export default Divider;
